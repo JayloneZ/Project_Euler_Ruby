a84983bf14b7e00e425d7262885d2ca060aff400
JayloneZ <andrezyczkowski@gmail.com>
2017-10-03 10:17:53 +0100

    added problem descriptions

--- prime_facts.rb
+++ prime_facts.rb
@@ -1,5 +1,9 @@
 require 'prime'
 
+"The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?"
+
 def prime_facts(divisible_number)
   for i in 1...divisible_number
     if divisible_number % i == 0
